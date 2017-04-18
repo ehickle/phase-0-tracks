0899856a46c5b4c03d58cc53bc0c9622151a956e
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-04-18 18:50:55 -0500

    Add decryption method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -12,8 +12,7 @@ word =str1
 word.downcase!
 count =0
 while count < word.length
-	if word[count] == " "
-		word[count] = " "
+	if word[count] == " "	
 	else
 	word[count] =word[count].next
 	end
@@ -23,8 +22,26 @@ puts word
 end
 
 encrypt("red blue")
+
 #Decrypt
 	#Ask user for input to be decrypted
 	#determine length of string
 	#use while loop to reverse each letter one a time
 	#display result
+def decrypt(str2)
+	word =str2
+	count=0
+	alphabet="abcdefghijklmnopqrstuvwxyz"
+	while count < word.length
+		if word[count] == " "
+		else
+		num=alphabet.index(word[count])
+		num = num-1
+		word[count]=alphabet[num]
+		end
+		count = count+1
+	end
+	puts word
+end
+decrypt("ebe b")
+
