b3728b0f99d8af434db83f38d6150ad97f9a403a
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-04-27 15:22:58 -0500

    Add release 2, add 4 calls to manipulate arrays and hashes with methods and blocks

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -31,3 +31,49 @@ City_rank.each do |city, rank|
 	puts "#{city} is the #{rank} biggest city in America"
 end
 
+
+#Release 2
+#1
+integers = [1,2,3,4,5,6,7,8,9,10]
+p integers
+letter_order = {a:1, b:2, c:3, d:4, e:5}
+p letter_order
+integers.reject! {|n| n<3}
+p integers
+letter_order.reject! {|letter, number| number<3}
+p letter_order
+
+#2
+integers2 = [1,2,3,4,5,6,7,8,9,10]
+p integers2
+letter_order2 = {a:1, b:2, c:3, d:4, e:5}
+p letter_order2
+integers2.select! {|n| n<3}
+p integers2
+letter_order2.select! {|letter, number| number < 3}
+p letter_order2
+
+#3
+integers3 = [1,2,3,4,5,6,7,8,9,10]
+p integers3
+letter_order3 = {a:1, b:2, c:3, d:4, e:5}
+p letter_order3
+integers3.keep_if {|n| n<5}
+p integers3
+letter_order3.keep_if {|letter, number| number < 5}
+p letter_order3
+
+#4
+integers4 = [1,2,3,4,5,6,7,8,9,10]
+p integers4
+letter_order4 = {a:1, b:2, c:3, d:4, e:5, f:6, g:7, h:8}
+p letter_order4
+integers4 =integers4.drop_while{|n| n<7} #must reassign array because drop_while is non-destructive
+p integers4
+letter_order4 =letter_order4.drop_while{|letter, number| number < 7}
+p letter_order4
+
+
+
+
+
