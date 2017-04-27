c6fa8595b5fec8ee13e6e5a7403a2c17338626a4
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-04-27 14:45:12 -0500

    Add .each and .map! calls to iterate through fruit array and city hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,5 +1,33 @@
+#Release 0
 def test_method
 	puts "This message displays due to the method"
 	yield("Evan")
 end
 test_method{|name| puts "#{name} inserted this message with a block"}
+
+
+
+#Release 1
+fruits =["apple", "orange" , "banana"]
+p fruits	#Display original array
+
+#Use each on an array
+fruits.each do |fruit|
+	fruit= fruit.upcase!
+end
+p fruits #print new array after each call
+
+fruits.map! do |fruit|
+	fruit.reverse
+end
+p fruits  #Print new array after .map! call
+
+
+City_rank = {Chicago: "3rd", Boston: "24th", Austin: "11th"}
+#An array showing listing  US cities and their rank by population
+
+#Use .each to run through a hash
+City_rank.each do |city, rank|
+	puts "#{city} is the #{rank} biggest city in America"
+end
+
