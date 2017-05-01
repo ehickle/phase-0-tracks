6dde689032e7c4016a96b2f0f5eba3fcd6b11294
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-04-30 22:49:13 -0500

    Complete iteration drill

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -5,21 +5,49 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
-# ----
+zombie_apocalypse_supplies.each {|supply| print supply, " * "}
+
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
-# ----
+# zombie_apocalypse_supplies.each do |first|
+# 	zombie_apocalypse_supplies.each do |second|
+#     alphabet= "abcdefghijklmnopqrstuvwxyz"
+# 		if alphabet.index(zombie_apocalypse_supplies[first[1]]) < alphabet.index(zombie_apocalypse_supplies[second[1]])
+# 			zombie_apocalypse_supplies[first] =zombie_apocalypse_supplies[second]
+# 			zombie_apocalypse_supplies[second] =zombie_apocalypse_supplies[first]
+# 		end
+# 	end
+# end
+
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
+zombie_apocalypse_supplies.each do |supply|
+  includes = "hatchet"
+  if supply == includes
+    puts "#{includes} is included"
+  else
+
+  end
+end
+
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
+
+zombie_apocalypse_supplies.each do |supply|
+i = zombie_apocalypse_supplies.length
+if i >5
+  zombie_apoalypse_supplies =zombie_apocalypse_supplies.delete(supply)
+end
+end
+p zombie_apocalypse_supplies
+
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -29,6 +57,8 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
+p total_supplies = (zombie_apocalypse_supplies + other_survivor_supplies).uniq
+
 
 # Hash Drills
 
@@ -44,17 +74,30 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+# ---
+extinct_animals.each {|key, value| puts "#{key}- #{value} * "}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
+# ---- 
+
+extinct_animals.each do |key, value|
+  if value >= 2000
+ extinct_animals.delete(key)
+  end
+end
+
+p extinct_animals
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+extinct_animals.each do |key, value|
+  value=value+3
+end
+p extinct_animals
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -63,9 +106,20 @@ extinct_animals = {
 # "Saiga Antelope"
 # Do not use any special built-in methods.
 # ----
+extinct_animals.each do |key, value|
+  if key == "Andean Cat"
+    print "Andean Cat is extinct"
+  elsif key == "Dodo"
+    print "Dodo is extinct"
+  elsif key == "Saiga Antelope"
+    print "Saiga Antelope is extinct"
+  end
+end
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+extinct_animals.delete("Passenger Pigeon") {|key, value| puts "#{key} - #{value}"}
+
