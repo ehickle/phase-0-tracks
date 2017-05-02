01aa96a55d3cf7e84f4d77f2eb176fbf11464993
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-01 22:53:19 -0500

    Add Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+#Puppies
+
+#CHARACTERISTICS ----------------
+#Size: small-medium
+#Friendly: yes
+#Eye count: 2
+#Cute: yes
+#Fur color: varies
+
+#BEHAVIOR -----------------------
+#Jump
+#bite
+#Be clumsy
+#Love humans
