c993dd74622341a5b496ef94b605ba7820b3d336
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-02 13:41:49 -0500

    Add one instance of puppy with five methods (tricks)

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(times)
+  	puts "Woof!"*times
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dogs_years = 7*human_years
+  	puts dogs_years
+  end
+
+  def play_dead
+  	puts "I'm super dead"
+  end
+
 end
 
+Kovu = Puppy.new
+Kovu.fetch("ball")
+Kovu.speak(3)
+Kovu.roll_over
+Kovu.dog_years(7)
+Kovu.play_dead
