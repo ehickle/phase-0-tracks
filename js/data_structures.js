6255f236342a1a96a0d51a45991bd9066da41241
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-08 10:06:00 -0500

    Create horse color array, and horse name array. Print both to console

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+colors = ["aquamarine", "Chartroeuse", "Amethyst Purple", "Caramel Brown"]
+horse_names  = ["Ill have another", "EZ does it", "Secretariat", "Lucky Strike"]
+colors.push("Jet Black")
+horse_names.push("Once in a Blue Moon")
+
+console.log(colors, horse_names)
