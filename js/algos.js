1ca05464a65e68023938e52475d35e4d64cef1b9
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-08 12:58:24 -0500

    Initialize function to detect longest string. Currently counts number of string input

---
+++ js/algos.js
@@ -0,0 +1,24 @@
+// Relase 0
+	// Spaces will count in the length of a string
+	// assign the length of each string to a unique position in an array
+	// Use loop to compare all given strings in an array of any length
+	// Inside loop, use logic statement to compare each word
+	// 	-First string is assigned to variable "longest"
+	// 	-If second string is longer, second string is assigned to variable "longest"
+	// 	-Continue until all words compared
+	// Return variable "longest"
+
+function longest_string(word_array) {
+	num_words = word_array.length
+	console.log(num_words)
+	// words = word_array
+	// word_lengths =[]
+	// for (i=0; i=num_words;i++)	{
+	// 	word_lengths[i] = (words[i]).length
+	// }
+	// return word_lengths
+}
+
+
+length_array = longest_string(["Taco", "Tacos", "Tacozz"])
+// console.log(length_array)
