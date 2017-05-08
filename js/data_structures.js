8ca931c33148e75f6851f91f8ad83cd6f63b42b3
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-08 10:17:10 -0500

    Add for loop to assign horses and color to an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,4 +3,14 @@ horse_names  = ["Ill have another", "EZ does it", "Secretariat", "Lucky Strike"]
 colors.push("Jet Black")
 horse_names.push("Once in a Blue Moon")
 
-console.log(colors, horse_names)
+// console.log(colors, horse_names)
+
+name_colors = {}
+// Release 1
+	// -Since our code only needs to work for arrays of the same length
+	// we can use a simple loop to create an object out of two arrays
+for (i=0; i<horse_names.length; i++) {
+name_colors[horse_names[i]] = colors[i]
+}
+
+console.log(name_colors)
