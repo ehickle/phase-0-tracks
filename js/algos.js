01885a8df454a44de4d9bcbb39737b099d901926
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-08 13:25:11 -0500

    Add second for loop in longest_string function. Compare length of each word to the length of the next word. Longest word is kept. Add driver code

--- js/algos.js
+++ js/algos.js
@@ -11,13 +11,30 @@
 function longest_string(word_array) {
 	num_words = word_array.length
 	word_lengths =[]
+	longest = word_array[0]
 
 	for (i=0; i<num_words;i++)	{
 		word_lengths.push(word_array[i].length)
 	}
-	console.log(word_lengths)
+
+	for (i=0; i<num_words; i++) {
+		if (word_lengths[i] < word_lengths[i+1]) {
+			longest = word_array[i+1]
+
+		} else if (word_lengths[i] = word_lengths[(i+1)]) {
+			longest = word_array[i] +" and " +word_array[i+1]
+
+		} else if (word_lengths[i] > word_lengths[i+1]) {
+			longest = word_array[i]
+		}
+		
+	}
+	return longest
 }
 
 
-length_array = longest_string(["Taco", "Tacos", "Tacozz"])
-// console.log(length_array)
+answer = longest_string(["Taco", "Tacos", "Tacozz"])
+console.log(answer)
+
+
+
