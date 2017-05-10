fa1cd8370a3109b5abd0eb53a5d674097bcdaa60
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-10 15:36:11 -0500

    Add pseudocode for key/value pair matching. Initialize function with no internal code

--- js/algos.js
+++ js/algos.js
@@ -20,11 +20,25 @@ function longest_string(word_array) {
 	longest_word = word_array[index]
 	return longest_word
 }
+
 answer = longest_string(["Tacozz", "Tacos", "taco", "burrito"])
 answer2 = longest_string(["joseph", "bob", "only", "hbbbb", "bbb"])
 console.log(answer)
 console.log(answer2)
 
 
+// Release 1
+	// Convert first object into two arrays, keys and values
+	// convert second object into two arrays, keys and values
+	// loop through each word in key array one
+	// 	-use "does include"? or similar function
+	// 		 to see if key exists in second key array
+	//  - if two keys match, proceed to next step
+	// Call the values of the matching keys in object 1 and 2
+	// -if two values match
+	// 	-function returns true
+	// -if not, funtion returns false
+
+	function share_pair(object)
 
 
