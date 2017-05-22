688f774e4f207e1b42381a4fd43e6bd000b4128c
Evan Hickle <evanhickle@Evans-MacBook-Pro.local>
2017-05-22 07:53:05 -0500

    Add loop to receive appropriate user input and store it in a local array

---
+++ databases/kitten_maker/marriage_bet.rb
@@ -0,0 +1,18 @@
+#The goal of this project is to create a functional back end program to store the
+#betting data. This is a continuation of the "Marriage Bet" website created earlier
+#in the course. The database will have 12 columns representing each person participating in the bet
+#each column will have twelve names. 
+
+require 'sqlite3'
+
+#Retrieve user input
+p "What is your name?"
+name= gets.chomp
+
+array = []
+i =0
+while i <12
+	puts "Who is your number #{i+1} pick?"
+	array[i] = gets.chomp
+	i = i+1
+end
